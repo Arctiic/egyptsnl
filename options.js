45f97931a510a7a7766fcdda4405f22873b3af5c
Arctiic <34424000+Arctiic@users.noreply.github.com>
2018-02-23 10:13:44 -0800

    Update options.js

--- options.js
+++ options.js
@@ -1,9 +1,21 @@
 const __OPTIONS = {
-  moves : [
+  cards : {
+    chances : [
+      "`Move {1, 8} squares forward`",
+      "`Move {1, 6} squares backward`",
+      "`Move {1, 8} squares forward then {1, 8} squares backward`",
+      "`Move {1, 8} squares backward then {1, 8} squares forward`",
+      "`Don't move.`"
+    ],
+
+    moves : [
+      "`Go to square {1, 36} of the current board`",
+      "`Go back {1, 4} squares`",
+      "`Go to square one. Sorry!`",
+      "`Move forward {1, 8} squares`",
+      "`Choose any player to go to square {1, 36} of either their board or your board`"
+    ]
+  }
   
-  ],
   
-  chances : [
-  
-  ]
 }
