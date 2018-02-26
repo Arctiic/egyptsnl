c0c68ed197d5a516506973409227f529b79810c5
Arctiic <34424000+Arctiic@users.noreply.github.com>
2018-02-26 14:51:14 -0800

    Update rng.js

--- rng.js
+++ rng.js
@@ -3,11 +3,19 @@ class RNG {
     
   }
   
-  percent () {
-    let arr = eval();
-    this.rng(0, 99);
+  percent (...args) {
+    let arr = "[";
+    let n this.rng(0, 99);
+    for (let i = 0; i < args.length; i++) {
+      let current = args[i];
+      for (let j = 0; j < current[1]) {
+      
+      }
+    }
+    arr += "]"
   }
   
   rng (mi, ma) {
+    return Math.floor((Math.random() * max) + min);
   }
 }
