473742afcf72dbb7c07aad2b7f2f0d3d81153331
Arctiic <34424000+Arctiic@users.noreply.github.com>
2018-02-27 14:35:26 -0800

    Update rng.js

--- rng.js
+++ rng.js
@@ -5,13 +5,14 @@ class RNG {
   
   percent (...args) {
     let arr = [];
-    let n this.rng(0, 99);
+    let n = this.rng(0, 99);
     for (let i = 0; i < args.length; i++) {
       let current = args[i];
       for (let j = 0; j < current[1]) {
         arr.push('' + current[0]);
       }
     }
+    retrun arr[n];
   }
   
   rng (min, max) {
