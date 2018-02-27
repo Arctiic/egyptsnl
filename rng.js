be38d9c30e2d8f65a06760159e528a6c03f3ff60
Arctiic <34424000+Arctiic@users.noreply.github.com>
2018-02-27 14:36:19 -0800

    Update rng.js

--- rng.js
+++ rng.js
@@ -8,7 +8,7 @@ class RNG {
     let n = this.rng(0, 99);
     for (let i = 0; i < args.length; i++) {
       let current = args[i];
-      for (let j = 0; j < current[1]) {
+      for (let j = 0; j < current[1]; j++) {
         arr.push('' + current[0]);
       }
     }
