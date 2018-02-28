0718fb1539326cc35e36939fcb268bb62a7edd1f
Arctiic <34424000+Arctiic@users.noreply.github.com>
2018-02-28 10:59:47 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-let chStyles = [
-	"`Go to square {1, 36} of the current board`",
-  "`Go back {1, 4} squares`",
-  "`Go to square one. Sorry!`",
-  "`Move forward {1, 8} squares`",
-  "`Choose any player to go to square {1, 36} of either their board or your board`"
-]
-
-let mvStyles = [
-	"`Move {1, 8} squares forward`",
-  "`Move {1, 6} squares backward`",
-  "`Move {1, 8} squares forward then {1, 8} squares backward`",
-  "`Move {1, 8} squares backward then {1, 8} squares forward`",
-  "`Don't move.`"
-]
+let chStyles = __OPTIONS.cards.chances;
+let mvStyles = __OPTIONS.cards.moves;
 
 let card = false;
 
